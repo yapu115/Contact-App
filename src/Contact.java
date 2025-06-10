d932c1d8c34bcddb3f54d3571b83107b1667efef
yapu_115 <yapufranco115@gmail.com>
2025-06-10 00:17:31 -0300

    Functional contact app

---
+++ src/Contact.java
@@ -0,0 +1,26 @@
+public class Contact {
+    private String name;
+    private String email;
+    private String phoneNumber;
+
+    public Contact(String name){
+        this.name = name;
+    }
+    public Contact(String name, String email){
+        this(name);
+        this.email = email;
+    }
+    public Contact(String name, String email, String phoneNumber){
+        this(name, email);
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String showInfo(){
+        return String.format("Name: %s - Email: %s - Phone number: %s", this.name, this.email, this.phoneNumber);
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,102 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Contact> contactList = new ArrayList<>();
+        System.out.println("-----------------------------");
+        System.out.println("Welcome to the contact list");
+        boolean showMenu = true;
+
+        while (showMenu){
+            ShowMenuOptions();
+            int numberSelected = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (numberSelected){
+                case 1:
+                    ShowAllContacts(contactList);
+                    break;
+                case 2:
+                    AddContact(scanner, contactList);
+                    break;
+                case 3:
+                    SearchContactByName(scanner, contactList);
+                    break;
+                case 4:
+                    DeleteContact(scanner, contactList);
+                    break;
+                case 5:
+                    showMenu = false;
+                    break;
+            }
+        }
+    }
+
+    static void ShowMenuOptions(){
+        System.out.println("Chose an option:");
+        System.out.println("1. Show all contacts");
+        System.out.println("2. Add new contact");
+        System.out.println("3. Search contact by name");
+        System.out.println("4. Delete contact");
+        System.out.println("5. Exit program");
+    }
+
+    static void ShowAllContacts(List<Contact> contactList){
+        for (Contact contact: contactList){
+            System.out.println(contact.showInfo());
+        }
+    }
+
+    static void AddContact(Scanner scanner, List<Contact> contactList){
+        System.out.println("Enter the information of the contact");
+
+        System.out.print("Name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Email: ");
+        String email = scanner.nextLine();
+
+        System.out.print("Phone number: ");
+        String phoneNumber = scanner.nextLine();
+
+        Contact newContact = new Contact(name, email, phoneNumber);
+        contactList.add(newContact);
+    }
+
+    static void SearchContactByName(Scanner scanner, List<Contact> contactList){
+        System.out.println("Enter the name of the contact: ");
+
+        System.out.print("Name: ");
+        String contactName = scanner.nextLine();
+
+        for (Contact contact: contactList){
+            if (contact.getName().contains(contactName)){
+                System.out.println("Contact found: ");
+                System.out.println(contact.showInfo());
+                return;
+            }
+        }
+        System.out.println("Contact not found");
+    }
+
+    static void DeleteContact(Scanner scanner, List<Contact> contactList){
+        System.out.println("What contact you wish to delete?: ");
+
+        int i = 1;
+        for (Contact contact: contactList){
+            System.out.printf("%d. %s%n", i, contact.getName());
+            i++;
+        }
+
+        int contactIndex = scanner.nextInt() - 1;
+        if (contactList.get(contactIndex) != null){
+            contactList.remove(contactIndex);
+            return;
+        }
+
+    }
+
+}
